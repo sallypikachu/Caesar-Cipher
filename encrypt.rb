27eb9086a10faabf79c6c86c758d13f37e1fdc28
sallypikachu <sallypikachu@gmail.com>
2016-02-17 12:16:41 -0500

    CaesarCipher and ASCIICipher

--- encrypt.rb
+++ encrypt.rb
@@ -1,11 +1,40 @@
-require 'pry'
-
-def encrypt_message(key, message)
-  encrypted_message = ''
-  message.split("").each do |letter|
-    encrypted_message += (letter.ord + key).chr
+module SecretMessage
+  def encrypt_message(message)
+    message.tr(@key, @codec)
   end
-  encrypted_message
+
+  def decrypt_message(message)
+    message.tr(@codec, @key)
+  end
 end
 
-puts encrypt_message(1, "Hi. My name is Sally!")
+class CaesarCipher
+  ALPHABET = ('a'..'z').to_a
+
+  def initialize(shift)
+    @key = 'a-zA-Z'
+    @shifted = ALPHABET.rotate(shift).join
+    @codec = @shifted + @shifted.upcase
+  end
+
+  include SecretMessage
+end
+
+class ComputerCipher
+  ASCII = ('!'..'~').to_a
+
+  def initialize(shift)
+    @key = ASCII.join
+    @codec = ASCII.rotate(shift).join
+  end
+
+  include SecretMessage
+end
+
+caesar = CaesarCipher.new(2)
+puts caesar.encrypt_message("Hi Larry!")
+puts caesar.decrypt_message(caesar.encrypt_message("Thanks for taking the time"))
+
+computer = ComputerCipher.new(3)
+puts computer.encrypt_message("to go through this interview process with me!")
+puts computer.decrypt_message(computer.encrypt_message("Cypher!!"))
